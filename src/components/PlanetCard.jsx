1bd524dc08b5b4d7c7f5f5bc77b506d6a78e2a01
Eric Santos <eric.silvasantos44@gmail.com>
2022-02-18 17:35:43 -0300

    Adicionado o sexto requisito

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class PlanerCard extends React.Component {
   render() {
-    const { planetName, planetImage } = this.props
+    const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
-        <img src={planetImage} alt={`Planeta ${planetName}`} />
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
         <p data-testid="planet-name">{planetName}</p>
       </div>
     );
   }
 }
+
+PlanerCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
 
 export default PlanerCard;
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Title from './Title';
+import PlanerCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
-        {/* <p>Teasdasst</p> */}
+        {planets.map(({ name, image }) => (
+          <PlanerCard key={ name } planetName={ name } planetImage={ image } />
+        ))}
       </div>
     );
   }
