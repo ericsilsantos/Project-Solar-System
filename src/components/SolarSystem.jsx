90a87fba420d20a3a723bbfcb0fdcea2e6a181e8
Eric Santos <eric.silvasantos44@gmail.com>
2022-02-18 16:14:50 -0300

    Adicionado o terceiro e quarto requisito

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <p>Teasdasst</p>
+        <Title headline="Planetas" />
+        {/* <p>Teasdasst</p> */}
       </div>
     );
   }
